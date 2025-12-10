d3419ac002060fcd0716b906eaf1b1f78a1e9683
Valery Rokka <mrrockka@gmail.com>
2025-12-10 18:20:09 +0100

    Task#38 Webapp poc Added fix for subheader background color https://github.com/mui/material-ui/issues/30468

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import {createBrowserRouter, RouterProvider} from "react-router";
 import {StrictMode} from "react";
 import {Dashboard} from "./pages/dashboard/Dashboard.jsx";
+import {alpha, createTheme, ThemeProvider} from "@mui/material";
 
 const router = createBrowserRouter([
     {
@@ -11,12 +12,31 @@ const router = createBrowserRouter([
         element: <App/>
     }, {
         path: "/dashboard",
-        element: <Dashboard />
+        element: <Dashboard/>
     }
 ])
 
+export const OVERLAY_ALPHA = 0.16
+
+const theme = createTheme({
+    colorSchemes: {
+        dark: true
+    },
+    components: {
+        MuiPaper: {
+            styleOverrides: {
+                root: {
+                    backgroundImage: `linear-gradient(${alpha('#fff', OVERLAY_ALPHA)}, ${alpha('#fff', OVERLAY_ALPHA)})`
+                }
+            }
+        }
+    }
+})
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <RouterProvider router={router}/>
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={router}/>
+        </ThemeProvider>
     </StrictMode>
 )
